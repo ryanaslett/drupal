84f21dab4b2e676dd98cc0bc944d7d5d8a16af2c
Daniel Wehner <dawehner@googlemail.com>
2017-12-11 22:32:24 +0000

    Improve initial setup

--- core/tests/Drupal/Nightwatch/globals.js
+++ core/tests/Drupal/Nightwatch/globals.js
@@ -2,6 +2,8 @@ const chromedriver = require('chromedriver');
 
 module.exports = {
   before: (done) => {
+    // Setting up 
+    process.env.SIMPLETEST_BASE_URL = process.env.SIMPLETEST_BASE_URL || process.env.BASE_URL;
     if (process.env.NODE_ENV !== 'testbot') {
       chromedriver.start();
     }

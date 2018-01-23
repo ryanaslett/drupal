c361b0b0ce7b15e19f9d08c00b4f0fde8a83992f
Daniel Wehner <dawehner@googlemail.com>
2018-01-23 19:44:49 +0000

    Ensure env variables win

--- core/tests/Drupal/Nightwatch/globals.js
+++ core/tests/Drupal/Nightwatch/globals.js
@@ -4,7 +4,7 @@ const settings = require('../../../nightwatch.settings.json');
 module.exports = {
   before: (done) => {
     // Setting up 
-    const baseUrl = settings.BASE_URL || process.env.SIMPLETEST_BASE_URL || process.env.BASE_URL;
+    const baseUrl = process.env.SIMPLETEST_BASE_URL || settings.BASE_URL || process.env.BASE_URL;
 
     if (baseUrl === undefined) {
       throw new Error('Missing a BASE_URL or SIMPLETEST_BASE_URL configuration item.');
@@ -14,7 +14,7 @@ module.exports = {
 
     // Note: The simpletest DB is optional, when there is a local drupal
     // installation.
-    process.env.SIMPLETEST_DB = settings.DB_URL || process.env.SIMPLETEST_DB;
+    process.env.SIMPLETEST_DB = process.env.SIMPLETEST_DB || settings.DB_URL;
 
     if (process.env.NODE_ENV !== 'testbot') {
       chromedriver.start();
